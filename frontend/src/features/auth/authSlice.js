2821a41b50ffc4eccf079d7b371cb54355fcaa56
Esmael <esmaelsabir9@gmail.com>
2025-07-24 16:00:27 +0300

    Add token handling in auth slice for user authentication

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,11 +1,25 @@
 
 import { createSlice } from '@reduxjs/toolkit';
+import { jwtDecode } from "jwt-decode";
 
 const initialState = {
   user: null,
   token: null,
   isAuthenticated: false,
 };
+
+const token = localStorage.getItem("token");
+if (token) {
+  const decodedToken = jwtDecode(token);
+  initialState.token = token;
+  initialState.isAuthenticated = true;
+  initialState.user = {
+    email: decodedToken.email,
+    role: decodedToken.role,
+    id: decodedToken.id,
+  };
+}
+
 
 const authSlice = createSlice({
   name: 'auth',
@@ -20,6 +34,7 @@ const authSlice = createSlice({
       state.user = null;
       state.token = null;
       state.isAuthenticated = false;
+      localStorage.removeItem("token");
     },
   },
 });
