eeb9bc9ca39b00a502fcd333ec0c72dfd68e2e71
Esmael <esmaelsabir9@gmail.com>
2025-07-24 14:16:03 +0300

    Add validation middleware for signup and login requests

---
+++ backend/middleware/validators/authValidators.js
@@ -0,0 +1,15 @@
+
+import { body } from 'express-validator';
+
+export const signupValidation = [
+  body('username').notEmpty().withMessage('Username is required'),
+  body('email').isEmail().withMessage('Please provide a valid email'),
+  body('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long'),
+];
+
+export const loginValidation = [
+  body('email').isEmail().withMessage('Please provide a valid email'),
+  body('password').notEmpty().withMessage('Password is required'),
+];
